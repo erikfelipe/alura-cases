6d00d361d40d3bf571580a11948fd932986b2a97
erikfelipe <erikfelipebh@gmail.com>
2023-11-23 13:45:09 -0300

    Create pages

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Alura Cases - HomePage</h1>
+      <Link href="/teste">Teste</Link>
+    </div>
+  );
+}
---
+++ pages/teste.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Alura Cases - Teste</h1>
+      <Link href="/">Home</Link>
+    </div>
+  );
+}
